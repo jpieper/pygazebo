9b1ca9eed7321e2e66f1341868a834ca452aad14
Josh Pieper <jjp@pobox.com>
2014-06-10 21:50:56 -0400

    Explicitly mark trollius as a requirement.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     package_dir={'pygazebo': 'pygazebo'},
     include_package_data=True,
-    install_requires=['protobuf'],
+    install_requires=['protobuf', 'trollius'],
     license="Apache License 2.0",
     zip_safe=False,
     keywords='pygazebo',
